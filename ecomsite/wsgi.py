26286eb86e07e5cf71dc60dd1abe4f035168640c
Sami Zayn <108477198+samithedeveloper1@users.noreply.github.com>
2024-04-27 01:55:47 +0600

    Update wsgi.py

--- ecomsite/wsgi.py
+++ ecomsite/wsgi.py
@@ -5,6 +5,19 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+
+WSGI config for ecomsite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+WSGI config for ecomsite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
