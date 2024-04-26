b6e7c1b5b4fe92ec28ed22d002335ee8d1eafee8
Khan Jubayer <144028577+khan-jubayer@users.noreply.github.com>
2024-04-26 16:38:46 +0600

    Update admin.py

--- shop/admin.py
+++ shop/admin.py
@@ -5,3 +5,8 @@ from .models import Order
 
 admin.site.register(Product)
 admin.site.register(Order)
+
+"""
+Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+known printer took a galley of type and scrambled it to make a type specimen book. 
+""
