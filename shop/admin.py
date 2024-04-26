56b798c2e70c07001b301eca9a2803e02e6c5f61
Khan Jubayer <144028577+khan-jubayer@users.noreply.github.com>
2024-04-26 16:44:41 +0600

    Update admin.py

--- shop/admin.py
+++ shop/admin.py
@@ -9,4 +9,4 @@ admin.site.register(Order)
 """
 Lorem Ipsum is simply dummy text of the printing and typesetting industry.
 known printer took a galley of type and scrambled it to make a type specimen book. 
-""
+"""
